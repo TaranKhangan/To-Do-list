855e85270b48de5e9d29284656f5e62ca3b45ba6
Taran <khangantaran@gmail.com>
2025-03-24 22:24:19 +0530

    Add project files and initial structure

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from "./MyComponents/Header";
+import Todos from "./MyComponents/Todos";
+import Footer from "./MyComponents/Footer";
+
+function App() {
+  return (
+   <>
+     <Header title="MyTodosList"/>
+     <Todos/>
+     <Footer/>
+   </>
+  );
+}
+
+export default App;
+
+//searchBar={false} for header props..
---
+++ src/MyComponents/Todos.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Todos = () => {
+  return (
+    <div>
+      Todos works!!
+      
+    </div>
+  )
+}
+
+export default Todos
